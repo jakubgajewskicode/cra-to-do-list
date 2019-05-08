70c517147985d253e58c4e49d735e01f5a357323
Jakub Gajewski <jacobs.hacking@gmail.com>
2019-05-08 07:05:08 +0100

    Updates

---
+++ precache-manifest.e54a2cf821a841c5b47f33b4a61eed3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "99a3baa67f7617c2441c",
+    "url": "/cra-to-do-list/static/js/runtime~main.30e58439.js"
+  },
+  {
+    "revision": "8c86ef06898a349b2f9b",
+    "url": "/cra-to-do-list/static/js/main.7b0bf761.chunk.js"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/cra-to-do-list/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "8c86ef06898a349b2f9b",
+    "url": "/cra-to-do-list/static/css/main.7bf6a15b.chunk.css"
+  },
+  {
+    "revision": "f5f64f0f127b6c465d88b14cbfdf4c7e",
+    "url": "/cra-to-do-list/index.html"
+  }
+];
